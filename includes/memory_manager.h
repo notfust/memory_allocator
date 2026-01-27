60a2feae3fac2a2ec7e5c143e552f8d6c83778e7
notfust <bladeheap@gmail.com>
2026-01-27 12:25:26 +0300

    chore: add `memory_manager` main files
    Added empty files for future implementation

---
+++ includes/memory_manager.h
@@ -0,0 +1,9 @@
+/**
+ * \file memory_manager.h
+ * \date 14.01.2026
+ * \brief Memory manager for embedded systems
+ */
+#ifndef MEMORY_MANAGER_H_
+#define MEMORY_MANAGER_H_
+
+#endif /* MEMORY_MANAGER_H_ */
---
+++ sources/memory_manager.c
@@ -0,0 +1,7 @@
+/**
+ * \file memory_manager.c
+ * \date 14.01.2026
+ * \brief Implementation of memory manager for embedded systems
+ */
+
+#include "../includes/memory_manager.h"
